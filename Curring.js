ff257eb3a87f4c4ae485d82681de44aaab3d7022
EVANS <986981086@qq.com>
2016-02-11 15:59:34 +0800

    Curring.js

---
+++ Curring.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-02-11 15:58:58
+ * @version $Id$
+ */
+
+
+if(!Function.prototype.bind){
+
+	Function.prototype.bind=function(oThis){
+
+	if(typeof this!=='function'){
+
+		throw new TypeError('What is trying to be bound is not callable');	
+	}	
+
+	var aArgs=Array.prototype.slice.call(arguments,1),
+	
+	fToBind=this,
+	fNOP=function(){},
+	fBound=function(){
+		return fToBind.apply(this instanceof fNOP ? this : oThis,aArgs.concat(Array.prototype.slice.call(arguments)));		
+	}
+
+    fNOP.prototype=this.prototype;
+    fBound.prototype=new fNOP();
+
+    return fBound;	
+   };	
+
+}
